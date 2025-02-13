4da6a1ad4b51860585f9dc6b998af8fdc82b7951
kauan.costa <costakauanantonye@gmail.com>
2025-02-13 17:03:29 -0300

    Atualizando mock das leituras Adicioando mock de reator Adicioando mock de microoganismo

--- app/data_mock.rb
+++ app/data_mock.rb
@@ -11,19 +11,38 @@ before do
   headers 'Access-Control-Allow-Origin' => '*'
 end
 
-# Generate mock user data
-get '/data_mock/reactors' do
-  JSON.generate(
-  	 {
- 	    		id: Faker::Number.unique.number(digits: 5),
-					name: "Reator #{Faker::Number.between(from: 1, to: 12)}",
-					code: Faker::Number.unique.number(digits: 5),
-					pressure: Faker::Number.within(range: 0.00..100.00),
-					oxygen: Faker::Number.within(range: 0.000..100.000),
-					temperature: Faker::Number.within(range: -50.00..150.00),
-					ph: Faker::Number.within(range: 0.0..14.0),
-					concentration: Faker::Number.within(range: 0.0..100.0),
-					contaminants: Faker::Boolean.boolean(true_ratio: 0.2)
-		 }
-  )
+get '/mock_data/reactors/:id/reactor_logs' do |reactor_id|
+	JSON.generate(
+		{
+			id: Faker::Number.unique.number(digits: 5),
+			reactor_id: reactor_id,
+			microorganism_id: Faker::Number.unique.number(digits: 5),
+			pressure: Faker::Number.within(range: 0.10..0.50),
+			oxygen: Faker::Number.within(range: 0.00..0.11),
+			temperature: Faker::Number.within(range: 26.00..30.00),
+			ph: Faker::Number.within(range: 5.0..8.0),
+			concentration: Faker::Number.within(range: 0.0..100.0),
+			contaminants: Faker::Boolean.boolean(true_ratio: 0.2)
+		}
+	)
+end
+
+get '/mock_data/reactors/:id/data' do |id|
+	JSON.generate(
+		{
+			id: id,
+			name: "Reactor #{Faker::Number.between(from: 1, to: 12)}",
+			code: Faker::Number.unique.number(digits: 5),
+		}
+	)
+end
+
+get '/mock_data/microorganisms/:id/data' do |id|
+	JSON.generate(
+		{
+			id: id,
+			name: Faker::Creature::Animal.name,
+			code: Faker::Number.unique.number(digits: 5),
+		}
+	)
 end
