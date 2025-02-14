7a519c1f40dfe44a27bd01245c965a61065842a7
kauan.costa <costakauanantonye@gmail.com>
2025-02-14 11:40:10 -0300

    É isso gurizada

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,5 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "bootstrap/dist/js/bootstrap.bundle"
-import '@popperjs/core'
-import "bootstrap"
-// import "bootstrap/dist/js/bootstrap.bundle"
 import 'https://unpkg.com/@popperjs/core@2.11.8/dist/umd/popper.min.js';
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js";
-
+// import "@stimulus-components/auto-submit"
